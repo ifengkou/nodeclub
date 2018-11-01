515e06a68f00f0c16a78171bca4d0e409492a650
sloong <ifengkou@hotmail.com>
2018-11-01 10:04:36 +0800

    add censortool.py

---
+++ censortool.py
@@ -0,0 +1,22 @@
+
+import shutil
+
+
+#file_origin = "censortest.txt"
+file_origin = "node_modules/text-censor/keywords"
+file_back = 'node_modules/text-censor/keywords.bak'
+file_new = "node_modules/text-censor/keywords.new"
+
+shutil.copy(file_origin, file_back)
+
+with open(file_origin,'r') as r:
+    lines=r.readlines()
+with open(file_new,'w') as w:
+    for keyword in lines:
+        keyword = keyword.strip('\n')
+        if not (keyword.isalpha() and len(keyword)<=3 ):
+            w.write(keyword+"\n") 
+        else:
+            print(keyword)
+
+shutil.move(file_new, file_origin)
