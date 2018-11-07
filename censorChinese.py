5694a19787c0b00fc5195f8ba295ac0cda369315
sloong <ifengkou@hotmail.com>
2018-11-07 11:14:57 +0800

    end剔除敏感词中的单个中文

--- censorChinese.py
+++ censorChinese.py
@@ -7,8 +7,12 @@ sys.setdefaultencoding('utf8')
 import shutil
 
 
-file_origin = "text.txt"
-#file_origin = "node_modules/text-censor/keywords"
+#file_origin = "text.txt"
+file_origin = "node_modules/text-censor/keywords"
+file_back = 'node_modules/text-censor/keywords.bak1'
+file_new = "node_modules/text-censor/keywords.new1"
+
+shutil.copy(file_origin, file_back)
 
 def is_chinese(uchar):
     if uchar >= u'\u4e00' and uchar<=u'\u9fa5':
@@ -18,9 +22,13 @@ def is_chinese(uchar):
 
 with open(file_origin,'r') as r:
     lines=r.readlines()
+
+with open(file_new,'w') as w:
     for keyword in lines:
-        print(len(keyword))
         keyword = keyword.strip().strip('\n')
         if is_chinese(keyword) and len(keyword) == 3:
             print(keyword)
+        else:
+            w.write(keyword+"\n") 
 
+shutil.move(file_new, file_origin)
