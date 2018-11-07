769db3b6b1471cabfa4f3e4cbe70cf86f1328ead
sloong <ifengkou@hotmail.com>
2018-11-07 11:04:59 +0800

    chinese char censort

--- censorChinese.py
+++ censorChinese.py
@@ -7,8 +7,8 @@ sys.setdefaultencoding('utf8')
 import shutil
 
 
-#file_origin = "text.txt"
-file_origin = "node_modules/text-censor/keywords"
+file_origin = "text.txt"
+#file_origin = "node_modules/text-censor/keywords"
 
 def is_chinese(uchar):
     if uchar >= u'\u4e00' and uchar<=u'\u9fa5':
@@ -19,7 +19,8 @@ def is_chinese(uchar):
 with open(file_origin,'r') as r:
     lines=r.readlines()
     for keyword in lines:
-        keyword = keyword.strip('\n')
+        print(len(keyword))
+        keyword = keyword.strip().strip('\n')
         if is_chinese(keyword) and len(keyword) == 3:
             print(keyword)
 
