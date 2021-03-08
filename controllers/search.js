7a3d849e46f3cf2ffef25144f7f2c5b33145d02a
sloong <ifengkou@hotmail.com>
2021-03-08 20:33:41 +0800

    change search:bing.com

--- controllers/search.js
+++ controllers/search.js
@@ -1,5 +1,6 @@
 exports.index = function (req, res, next) {
   var q = req.query.q;
   q = encodeURIComponent(q);
-  res.redirect('https://www.google.com.hk/#hl=zh-CN&q=site:clickhouse.com.cn+' + q);
+  //res.redirect('https://www.google.com.hk/#hl=zh-CN&q=site:clickhouse.com.cn+' + q);
+  res.redirect('https://cn.bing.com/search?q=site:clickhouse.com.cn+' + q);
 };
